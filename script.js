39904908c0f604a3a9e95a222de676d2697ae259
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2025-11-11 19:39:17 -0800

    feat: bootstrap portfolio project with HTML, CSS, Js , and .gitignore

---
+++ script.js
@@ -0,0 +1,12 @@
+// Put current year in footer
+document.getElementById('year').textContent = new Data ().getFullYear();
+
+// Theme toggle (also modefile  style via Dom)
+const toggle = document.getElementById('theme-toggle');
+toggle.addEventListener('click', () =>{
+    document.body.classList.toggle('dark');
+});
+
+// obvison override of a CSS rule  at least once:
+const featuresCard = document.getElementById('featured');
+featuresCard.style.borderColor = '#7e3eff';
